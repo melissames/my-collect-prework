f0e780312f4794a49fbc551dee90bee058f123fe
Melissa Sames <melissa.sames@gmail.com>
2018-01-16 19:48:08 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 def my_collect (collection)
   i = 0
+  new_collection = []
 
   while i < collection.length
     yield collection[i]
+    new_collection << collection[i]
     i = i + 1
   end
-  collection
+  new_collection
 end
